7fbfbebac96175c9005dc14b68fbd8d91942b910
morkmork26 <157001614+morkmork26@users.noreply.github.com>
2026-07-27 14:13:24 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+var CACHE_NAME = 'rel-tracker-v1';
+var urlsToCache = [
+  '/rel-tracker/',
+  '/rel-tracker/index.html',
+  '/rel-tracker/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request).then(function(resp) {
+        if (!resp || resp.status !== 200) return resp;
+        var responseToCache = resp.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseToCache);
+        });
+        return resp;
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) { return name !== CACHE_NAME; })
+          .map(function(name) { return caches.delete(name); })
+      );
+    })
+  );
+});
